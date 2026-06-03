f4f36af2edc7bc1bc0f4de2bccee68fbdcb04079
paul <paul.chazalmartin@protonmail.com>
2026-06-03 11:36:15 +0200

    ex02

--- Cpp_06/ex02/src/main.cpp
+++ Cpp_06/ex02/src/main.cpp
@@ -1,29 +1,69 @@
 
-#include "../includes/Serializer.hpp"
-#include "../includes/Data.hpp"
+#include "../includes/Base.hpp"
+#include "../includes/A.hpp"
+#include "../includes/B.hpp"
+#include "../includes/C.hpp"
+
 #include <iostream>
+#include <cstdlib>
+
+Base *generate(void)
+{
+	int i = rand() % 3;
+	if (i == 0)
+		return new A();
+	if (i == 1)
+		return new B();
+	if (i == 2)
+		return new C();
+	return (NULL);
+}
+
+void identify(Base *p)
+{
+	if (dynamic_cast<A *>(p))
+		std::cout << "type is : A" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "type is : B" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "type is : C" << std::endl;
+}
+
+void identify(Base &p)
+{
+	try {
+		A &a = dynamic_cast<A &>(p);
+		std::cout << "type is : A" << std::endl;
+		(void)a;
+	}
+	catch(std::exception &e) {}
+
+	try {
+		B &b = dynamic_cast<B &>(p);
+		std::cout << "type is : B" << std::endl;
+		(void)b;
+	}
+	catch(std::exception &e) {}
+
+	try {
+		C &c = dynamic_cast<C &>(p);
+		std::cout << "type is : C" << std::endl;
+		(void)c;
+	}
+	catch(std::exception &e) {}
+}
 
 int main(void)
 {
-    Data data;
-    data.number = 42;
-    data.name = "hello";
+	srand(time(NULL) ^ clock());
+	Base *base_find = generate();
 
-    std::cout << "Original pointer : " << &data << std::endl;
-
-    uintptr_t raw = Serializer::serialize(&data);
-    std::cout << "Serialized       : " << raw << std::endl;
-
-    Data *result = Serializer::deserialize(raw);
-    std::cout << "Deserialized     : " << result << std::endl;
-
-    if (result == &data)
-        std::cout << "OK : pointeurs identiques !" << std::endl;
-    else
-        std::cout << "KO : pointeurs differents !" << std::endl;
-
-    std::cout << "data.number : " << result->number << std::endl;
-    std::cout << "data.name   : " << result->name << std::endl;
-
-    return 0;
+	std::cout << "Base *p" << std::endl;
+	identify(base_find);
+	std::cout << std::endl;
+	std::cout << "Base &p" << std::endl;
+	Base &base_ref = *base_find;
+	identify(base_ref);
+	std::cout << std::endl;
+	delete base_find;
 }
